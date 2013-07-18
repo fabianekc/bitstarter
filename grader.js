61fa10f178d125c2ca79753e9f87df47a7daf7d8
Christoph Fabianek <christoph.fabianek@gmail.com>
2013-07-18 22:25:26 +0000

    bootstrap responsive design

--- grader.js
+++ grader.js
@@ -27,7 +27,7 @@ var cheerio = require('cheerio');
 var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var HTMLURL_DEFAULT = "http://cryptic-earth-6515.herokuapp.com/index.html";
+var HTMLURL_DEFAULT = "http://cryptic-earth-6515.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -53,6 +53,18 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioHtmlUrl = function(htmlurl) {
+    rest.get(htmlurl.toString()).on('complete', x=function(result) {
+        if (result instanceof Error) {
+            console.log('Error: ' + result.message);
+            process.exit(1);
+        } else {
+            return cheerio.load(result);
+        }
+    });
+    return x;
+};
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
@@ -69,23 +81,14 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 };
 
 var checkHtmlUrl = function(htmlurl, checksfile) {
-    console.log(htmlurl);
-    rest.get(htmlurl.toString()).on('complete', function(result) {
-        if (result instanceof Error) {
-            sys.puts('Error: ' + result.message);
-            process.exit(1);
-        } else {
-            console.log(result);
-            $ = result;
-            var checks = loadChecks(checksfile).sort();
-            var out = {};
-            for(var ii in checks) {
-                var present = $(checks[ii]).length > 0;
-                out[checks[ii]] = present;
-            }
-            return out;
-        }
-    });
+    $ = cheerioHtmlUrl(htmlurl);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
 };
 
 
@@ -98,12 +101,19 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-u, --url <html_url>', 'URL of index.html', clone(assertUrlExists), HTMLURL_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <html_url>', 'URL of index.html', clone(assertUrlExists))
         .parse(process.argv);
-    // var checkJson = checkHtmlFile(program.file, program.checks);
-    console.log(program.url);
-    var checkJson = checkHtmlUrl(program.url, program.checks);
+    if (program.file) {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+    } else {
+        if (program.url) {
+            var checkJson = checkHtmlUrl(program.url, program.checks);
+        } else {
+            console.log('Error: missing arguments');
+            process.exit(1);
+        }
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
