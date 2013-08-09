a4ff74d48425347ba408574d02cccc82ef699353
fabianekc <chrsitoph.fabianek@gmail.com>
2013-08-09 10:10:07 +0000

    new files for indepth infos

--- web.js
+++ web.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var htmlfile = "index.html";
+var usecases_html = "usecases.html"
+var developers_html = "developers.html"
+var investors_html = "investors.html"
 
 var app = express.createServer(express.logger());
 
@@ -8,6 +11,16 @@ app.get('/', function(request, response) {
   var html = fs.readFileSync(htmlfile).toString();
   response.send(html);
 });
+app.get('/usecases', function(request, response) {
+  response.send(fs.readFileSync(usecases_html).toString());
+});
+app.get('/developers', function(request, response) {
+  response.send(fs.readFileSync(usecases_html).toString());
+});
+app.get('/investors', function(request, response) {
+  response.send(fs.readFileSync(usecases_html).toString());
+});
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
